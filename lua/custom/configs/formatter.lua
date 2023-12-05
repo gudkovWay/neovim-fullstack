ebd439d4bc1a5d42936c92d912b5869f161b53f9
GudkovIurii <147748630+gudkovWay@users.noreply.github.com>
2023-12-05 11:51:00 +0000

    Add files via upload

---
+++ lua/custom/configs/formatter.lua
@@ -0,0 +1,28 @@
+local M = {
+  filetype = {
+    javascriptreact = {
+      require("formatter.filetypes.javascriptreact").prettier,
+    },
+    typescriptreact = {
+      require("formatter.filetypes.typescriptreact").prettier,
+    },
+    javascript = {
+      require("formatter.filetypes.javascript").prettier,
+    },
+    typescript = {
+      require("formatter.filetypes.typescript").prettier,
+    },
+    css = {
+      require("formatter.filetypes.css").prettier,
+    },
+    ["*"] = {
+      require("formatter.filetypes.any").remove_trailing_whitespace,
+    },
+  },
+}
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  command = "FormatWriteLock",
+})
+
+return M
